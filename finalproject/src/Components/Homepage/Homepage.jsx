9458eaea95aa602fa9558babdfeef3d02ab74425
Nicholas <blueretrograde@gmail.com>
2022-12-06 11:13:00 +0000

    first stage

--- finalproject/src/Components/Homepage/Homepage.jsx
+++ finalproject/src/Components/Homepage/Homepage.jsx
@@ -1,11 +1,12 @@
 import "./Homepage.scss"
 import map from "../../Assets/images/world-map.gif"
+import sf from "../../Assets/images/San Francisco.JPG"
 
 const Homepage = () => {
     return(
         <div className="homepage">
-            <h4 className="homepage__title">Pick a destination!</h4>
-            <img className="homepage__photo" src={map} alt="world map" />
+            <h4 className="homepage__title">San Franciso, California</h4>
+            <img className="homepage__photo" src={sf} alt="world map" />
         </div>
     )
 };
