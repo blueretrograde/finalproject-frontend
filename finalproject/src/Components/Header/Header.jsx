8c5d4549b49d35faa4cd3629a3a8326528b2a9a0
Nicholas <blueretrograde@gmail.com>
2022-12-07 09:27:39 +0000

    minor changes

--- finalproject/src/Components/Header/Header.jsx
+++ finalproject/src/Components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <div className="header">
 
             <div className="header__name">
-                <h3 className="header__name--title">Untitled</h3>
+                <h3 className="header__name--title">T R A V E L .</h3>
             </div>
 
             <div className="header__buttons"> 
